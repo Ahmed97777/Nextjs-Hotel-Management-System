981d490174eeafe31423039e484550978f918daf
Ahmed Khorkhash <khorkhash3@gmail.com>
2024-06-01 14:51:17 +0300

    Make filter feature components reusable.

--- components/CabinsComponents/CabinTableOperations.js
+++ components/CabinsComponents/CabinTableOperations.js
@@ -6,7 +6,14 @@ import Filter from "../Filter";
 export default function CabinTableOperations() {
   return (
     <div className="flex items-center gap-[1.6rem]">
-      <Filter />
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No discount" },
+          { value: "with-discount", label: "With discount" },
+        ]}
+      />
     </div>
   );
 }
--- components/Filter.js
+++ components/Filter.js
@@ -1,33 +1,30 @@
 import React from "react";
 import FilterButton from "./FilterButton";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
-export default function Filter() {
+export default function Filter({ filterField, options }) {
   const router = useRouter();
 
   function handleClick(value) {
     const params = new URLSearchParams();
-    params.set("discount", value);
+    params.set(filterField, value);
     router.push(`?${params.toString()}`);
   }
 
+  const searchParams = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options?.[0].value;
+
   return (
     <div className="border border-gray-100 border-solid bg-white shadow-sm rounded-sm sm:p-1 flex gap-1 sm:gap-4">
-      <FilterButton active="all" onClick={() => handleClick("all")}>
-        All
-      </FilterButton>
-      <FilterButton
-        active="no-discount"
-        onClick={() => handleClick("no-discount")}
-      >
-        No-discount
-      </FilterButton>
-      <FilterButton
-        active="with-discount"
-        onClick={() => handleClick("with-discount")}
-      >
-        With-discount
-      </FilterButton>
+      {options?.map((option) => (
+        <FilterButton
+          key={option.value}
+          active={option.value === currentFilter}
+          onClick={() => handleClick(option.value)}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
     </div>
   );
 }
