d488b0ed2c2b4e6881332ebdd3d82e1dc9014f59
Ahmed Khorkhash <khorkhash3@gmail.com>
2024-06-01 13:57:03 +0300

    Feature: Add discount filter for cabins.

--- components/CabinsComponents/CabinsContent.js
+++ components/CabinsComponents/CabinsContent.js
@@ -10,12 +10,21 @@ export default function CabinsContent() {
   const { cabinsData } = useGetCabins();
   const searchParams = useSearchParams();
   const discountFilter = searchParams.get("discount");
-  console.log(discountFilter);
+
+  const filterDiscountValue = discountFilter || "all";
+  console.log(filterDiscountValue);
+
+  let filteredCabins;
+  if (filterDiscountValue === "all") filteredCabins = cabinsData;
+  if (filterDiscountValue === "no-discount")
+    filteredCabins = cabinsData?.filter((cabin) => cabin.discount === 0);
+  if (filterDiscountValue === "with-discount")
+    filteredCabins = cabinsData?.filter((cabin) => cabin.discount > 0);
 
   return (
     <>
       {Array.isArray(cabinsData) ? (
-        cabinsData.map((cabin) => <CabinRow key={cabin.id} cabin={cabin} />)
+        filteredCabins.map((cabin) => <CabinRow key={cabin.id} cabin={cabin} />)
       ) : cabinsData === "Cabins could not be loaded" ? (
         <div role="row">
           <FetchWarning message="Warning: there have been a problem with fetching cabins, please refresh the page." />
