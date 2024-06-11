ba9b189d047dc23119589bf09d394ccfc900c57a
Ahmed Khorkhash <khorkhash3@gmail.com>
2024-06-11 15:56:27 +0300

    Enhance booking table design.

--- components/BookingComponents/BookingRow.js
+++ components/BookingComponents/BookingRow.js
@@ -43,15 +43,15 @@ function BookingRow({
       key={bookingId}
       className="grid grid-cols-[1fr,2fr,2.4fr,1.4fr,1fr,3.2rem] text-xs sm:text-sm p-2 sm:p-4 items-center border-b border-solid border-gray-100 last:border-none"
     >
-      <div className="w-14 font-semibold text-gray-600">{cabinName}</div>
+      <div className="font-semibold text-gray-600">{cabinName}</div>
 
-      <div className="w-28 flex flex-col gap-1">
+      <div className="flex flex-col gap-1">
         <span className="text-gray-600 font-medium">{guestName}</span>
 
         <span className="text-gray-500">{email}</span>
       </div>
 
-      <div className="w-28 flex flex-col gap-1">
+      <div className="flex flex-col gap-1">
         <span className="text-gray-600 font-medium">
           {isToday(new Date(startDate))
             ? "Today"
@@ -66,12 +66,12 @@ function BookingRow({
       </div>
 
       <div
-        className={`w-28 uppercase text-center p-1 font-semibold rounded-full ${textColorClass[statusType]} ${bgColorClass[statusType]} `}
+        className={`capitalize text-center p-1 font-semibold rounded-full ${textColorClass[statusType]} ${bgColorClass[statusType]} `}
       >
         {status.replace("-", " ")}
       </div>
 
-      <div className="w-28 font-medium text-green-700">
+      <div className="font-medium text-green-700">
         {formatCurrency(totalPrice)}
       </div>
     </div>
--- components/BookingComponents/BookingTable.js
+++ components/BookingComponents/BookingTable.js
@@ -6,18 +6,18 @@ function BookingTable() {
     <div
       /*Cabins Table*/
       role="table"
-      className="overflow-auto min-w-[310px] max-w-[900px] border-2 border-solid border-gray-200 bg-white rounded-lg"
+      className="overflow-auto min-w-[500px] max-w-[900px] border-2 border-solid border-gray-200 bg-white rounded-lg"
     >
       <header
         role="row"
-        className="grid grid-cols-[1fr,2fr,2.4fr,1.4fr,1fr,3.2rem] p-2 sm:p-4 bg-gray-50 border-2 border-solid border-gray-100 text-xs sm:text-sm md:text-base font-semibold text-gray-600"
+        className="grid grid-cols-[1fr,2fr,2.4fr,1.4fr,1fr,3.2rem] p-2 sm:p-4 bg-gray-50 border-2 border-solid border-gray-100 text-xs sm:text-sm font-semibold text-gray-600"
       >
-        <div className="w-14">Cabin</div>
-        <div className="w-28">Guest</div>
-        <div className="w-28">Dates</div>
-        <div className="w-28">Status</div>
-        <div className="w-28">Amount</div>
-        <div className="w-28"></div>
+        <div className="">Cabin</div>
+        <div className="">Guest</div>
+        <div className="">Dates</div>
+        <div className="">Status</div>
+        <div className="">Amount</div>
+        <div className=""></div>
       </header>
 
       <BookingsContent />
