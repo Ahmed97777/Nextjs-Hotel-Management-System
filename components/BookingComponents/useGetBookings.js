7f4110b24ecfa209cf24d3b2014c3e520467ce5f
Ahmed Khorkhash <khorkhash3@gmail.com>
2024-06-15 13:56:27 +0300

    Finish pre-fetching feature for bookings data table to enhance user experience.

--- components/BookingComponents/useGetBookings.js
+++ components/BookingComponents/useGetBookings.js
@@ -1,14 +1,17 @@
 import { getBookings } from "@/services/apiBookings";
-import { useQuery } from "@tanstack/react-query";
+import { PAGE_SIZE } from "@/utils/constants";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { useSearchParams } from "next/navigation";
 
 export function useGetBookings() {
+  const queryClient = useQueryClient();
   const searchParams = useSearchParams();
 
-  // Pagination:
+  // PAGINATION:
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // QUERY
   const {
     data: { data: bookingsData, count } = {},
     isLoading,
@@ -17,6 +20,25 @@ export function useGetBookings() {
     queryKey: ["bookings", page],
     queryFn: () => getBookings({ page }),
   });
+
+  // PRE-FETCHING
+  // 1) NEXT PAGE
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", page + 1],
+      queryFn: () => getBookings({ page: page + 1 }),
+    });
+  }
+
+  // 2) PREV PAGE
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", page - 1],
+      queryFn: () => getBookings({ page: page - 1 }),
+    });
+  }
 
   return { bookingsData, count, isLoading, error };
 }
